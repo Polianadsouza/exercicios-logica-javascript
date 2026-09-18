1ac1c4f8f372eb48b5a05034651da495d731a61e
Poliana Souza <polianareisdesouza@gmail.com>
2026-09-18 20:32:47 -0300

    Adicionar exercicios

---
+++ cap02/js/ex2_4.js
@@ -0,0 +1,14 @@
+//cria referência ao form e ao elemento h3 onde será exiboda a resposta)
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//cria um "ouvinte" de elemento, acionado quando o botao submit for clicado
+frm.addEventListener("submit" , (e) => {
+    const quilo = Number(frm.inQuilo.value)
+    const consumo = Number(frm.inConsumo.value)
+
+    const valor = (quilo / 1000) *  consumo
+    resp.innerText = `Valor a pagar R$: ${valor.toFixed(2)}`
+
+    e.preventDefault()
+})
